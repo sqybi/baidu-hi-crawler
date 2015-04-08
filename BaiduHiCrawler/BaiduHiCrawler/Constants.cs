c47a7b4d0b306de3b3b0ae694cfebde0ef803a6a
sqybi <sqybi@126.com>
2015-04-08 15:00:45 +0800

    Finish major logic and some of the functions

---
+++ BaiduHiCrawler/BaiduHiCrawler/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace BaiduHiCrawler
+{
+    using System;
+
+    static class Constants
+    {
+        public static readonly Uri LoginUri = new Uri("http://hi.baidu.com/go/login");
+
+        public static readonly Uri HomeUri = new Uri("http://hi.baidu.com/home");
+        //public static readonly Uri HomeUri = new Uri("http://dev.sqybi.com");
+    }
+}
--- BaiduHiCrawler/BaiduHiCrawler/MainWindow.xaml.cs
+++ BaiduHiCrawler/BaiduHiCrawler/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace BaiduHiCrawler
+﻿namespace BaiduHiCrawler
 {
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +15,90 @@ namespace BaiduHiCrawler
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonNavigateToLoginPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.WebBrowserCrawler.Navigate(Constants.LoginUri);
+        }
+
+        private async void ButtonStartCrawling_Click(object sender, RoutedEventArgs e)
+        {
+            string text;
+
+            // Get URI of hi space
+            var spaceLinkRegex = new Regex(
+                @"<A href=""(http://hi.baidu.com/[^""]+)"">我的主页</A>",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            if (!await this.NavigateWithRegexCheck(this.WebBrowserCrawler, Constants.HomeUri, spaceLinkRegex, null, out text))
+            {
+                return;
+            }
+            var spaceLink = spaceLinkRegex.Match(text).Groups[1].Value;
+            var spaceUri = new Uri(spaceLink);
+
+            // Get pages count
+            var pageCountRegex = new Regex(
+                @"<A class=last href=""" + spaceLink + @"\?page=(\d+)"">尾页</A>",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            var pageCountSucceedRegex = new Regex(
+                @"<DIV class=mod-pagerbar>",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            if (!await this.NavigateWithRegexCheck(this.WebBrowserCrawler, Constants.HomeUri, pageCountSucceedRegex, null, out text))
+            {
+                return;
+            }
+            var match = pageCountRegex.Match(text);
+            int totalPage;
+            if (!match.Success || !int.TryParse(match.Groups[1].Value, out totalPage))
+            {
+                totalPage = 1;
+            }
+
+            // Get articles on each page
+            for (int pageId = 1; pageId <= totalPage; pageId++)
+            {
+                var pageUri = new Uri(spaceLink + "?page=" + pageId);
+                var pageContentRegex = new Regex(""); // TODO: what regex?
+                if (!await this.NavigateWithRegexCheck(this.WebBrowserCrawler, Constants.HomeUri, pageContentRegex, null, out text))
+                {
+                    return;
+                }
+
+                // TODO: finish this
+            }
+        }
+
+        private async Task<bool> NavigateWithRegexCheck(WebBrowser webBrowser, Uri uri, Regex succeedRegex, Regex failedRegex, out string text)
+        {
+            if (succeedRegex == null)
+            {
+                text = string.Empty;
+                return false;
+            }
+
+            this.WebBrowserCrawler.Navigate(uri);
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                
+                dynamic htmlDoc = this.WebBrowserCrawler.Document;
+                text = htmlDoc.documentElement.outerHTML;
+
+                if (succeedRegex.Match(text).Success)
+                {
+                    return true;
+                }
+
+                if (failedRegex != null && failedRegex.Match(text).Success)
+                {
+                    text = string.Empty;
+                    return false;
+                }
+            }
+
+            text = string.Empty;
+            return false;
         }
     }
 }
