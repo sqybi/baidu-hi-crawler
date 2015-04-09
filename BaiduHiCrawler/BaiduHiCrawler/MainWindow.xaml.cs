2dc0d5673985624f2e7603cea399e2acb60d4d2f
sqybi <sqybi@126.com>
2015-04-09 20:21:52 +0800

    Add try catch and tips for crawling

--- BaiduHiCrawler/BaiduHiCrawler/MainWindow.xaml.cs
+++ BaiduHiCrawler/BaiduHiCrawler/MainWindow.xaml.cs
@@ -48,120 +48,130 @@ namespace BaiduHiCrawler
 
         private async void ButtonStartCrawling_Click(object sender, RoutedEventArgs e)
         {
-            // Get URI of hi space
-            var spaceLinkRegex = new Regex(
-                @"<A href=""(http://hi.baidu.com/[^""]+)"">我的主页</A>",
-                RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            var text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, Constants.HomeUri, spaceLinkRegex, null);
-            if (text == null)
+            try
             {
-                return;
-            }
-            var spaceLink = spaceLinkRegex.Match(text).Groups[1].Value;
-            var spaceUri = new Uri(spaceLink);
-
-            // Get pages count
-            var pageCountRegex = new Regex(
-                @"<A class=last href=""" + spaceLink + @"\?page=(\d+)"">尾页</A>",
-                RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            var pageCountSucceedRegex = new Regex(
-                @"<DIV class=mod-pagerbar>",
-                RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, spaceUri, pageCountSucceedRegex, null);
-            if (text == null)
-            {
-                return;
-            }
-            var match = pageCountRegex.Match(text);
-            int totalPage;
-            if (!match.Success || !int.TryParse(match.Groups[1].Value, out totalPage))
-            {
-                totalPage = 1;
-            }
-
-            // Get articles on each page
-            var articles = new List<Article>();
-            for (int pageId = 1; pageId <= totalPage; pageId++)
-            {
-                // Get article list
-                var pageUri = new Uri(spaceLink + "?page=" + pageId);
-                var pageContentRegex = new Regex(@"<A class=""a-incontent a-title cs-contentblock-hoverlink"" href=""([^""]+)"" target=_blank>[^<]*</A>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                var pageSucceedRegex = pageCountSucceedRegex;
-                text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, pageUri, pageSucceedRegex, null);
+                // Get URI of hi space
+                var spaceLinkRegex = new Regex(
+                    @"<A href=""(http://hi.baidu.com/[^""]+)"">我的主页</A>",
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                var text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, Constants.HomeUri, spaceLinkRegex, null);
                 if (text == null)
                 {
-                    continue;
+                    return;
+                }
+                var spaceLink = spaceLinkRegex.Match(text).Groups[1].Value;
+                var spaceUri = new Uri(spaceLink);
+
+                // Get pages count
+                var pageCountRegex = new Regex(
+                    @"<A class=last href=""" + spaceLink + @"\?page=(\d+)"">尾页</A>",
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                var pageCountSucceedRegex = new Regex(
+                    @"<DIV class=mod-pagerbar>",
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, spaceUri, pageCountSucceedRegex, null);
+                if (text == null)
+                {
+                    return;
+                }
+                var match = pageCountRegex.Match(text);
+                int totalPage;
+                if (!match.Success || !int.TryParse(match.Groups[1].Value, out totalPage))
+                {
+                    totalPage = 1;
                 }
 
-                var articleMatches = pageContentRegex.Matches(text);
-                foreach (Match articleMatch in articleMatches)
+                // Get articles on each page
+                var articles = new List<Article>();
+                for (int pageId = 1; pageId <= totalPage; pageId++)
                 {
-                    // Get single article link
-                    var articleLink = "http://hi.baidu.com" + articleMatch.Groups[1].Value;
-                    var articleUri = new Uri(articleLink);
-
-                    // Regexes
-                    var articleTitleRegex =
-                        new Regex(
-                            @"<h2 class=""title content-title"">([^<]*)</h2>",
-                            RegexOptions.Compiled | RegexOptions.IgnoreCase);
-                    var articleHtmlContentRegex =
-                        new Regex(
-                            @"<div id=content class=""content mod-cs-content text-content clearfix"">(.*)</div>\s+<div class=""mod-tagbox clearfix"">",
-                            RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
-
-                    // Get article page text
-                    text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, articleUri, articleTitleRegex, null);
+                    // Get article list
+                    var pageUri = new Uri(spaceLink + "?page=" + pageId);
+                    var pageContentRegex = new Regex(@"<A class=""a-incontent a-title cs-contentblock-hoverlink"" href=""([^""]+)"" target=_blank>[^<]*</A>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    var pageSucceedRegex = pageCountSucceedRegex;
+                    text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, pageUri, pageSucceedRegex, null);
                     if (text == null)
                     {
                         continue;
                     }
 
-                    // Get article
-                    var article = new Article();
-                    article.Title = articleTitleRegex.Match(text).Groups[1].Value;
-                    article.HtmlContent = articleHtmlContentRegex.Match(text).Groups[1].Value;
-                    
-                    // Get article comments (by json, not regex)
-                    article.Comments = new List<Comment>();
-                    var articleId = articleLink.TrimEnd('/').Substring(articleLink.TrimEnd('/').LastIndexOf('/') + 1);
-                    var articleCommentJsonText = GetCommentJsonText(articleId, 0, 1);
-                    var articleCommentJsonObject = JObject.Parse(articleCommentJsonText);
-                    var articleCommentCount = articleCommentJsonObject["data"][0]["total_count"].Value<int>();
-
-                    if (articleCommentCount > 0)
+                    var articleMatches = pageContentRegex.Matches(text);
+                    foreach (Match articleMatch in articleMatches)
                     {
-                        articleCommentJsonText = GetCommentJsonText(articleId, 0, articleCommentCount);
-                        articleCommentJsonObject = JObject.Parse(articleCommentJsonText);
-                        for (int commentId = 0; commentId < articleCommentCount; commentId++)
+                        // Get single article link
+                        var articleLink = "http://hi.baidu.com" + articleMatch.Groups[1].Value;
+                        var articleUri = new Uri(articleLink);
+
+                        // Regexes
+                        var articleTitleRegex =
+                            new Regex(
+                                @"<h2 class=""title content-title"">([^<]*)</h2>",
+                                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        var articleHtmlContentRegex =
+                            new Regex(
+                                @"<div id=content class=""content mod-cs-content text-content clearfix"">(.*)</div>\s+<div class=""mod-tagbox clearfix"">",
+                                RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                        // Get article page text
+                        text = await this.NavigateWithRegexCheck(this.webBrowserCrawler, articleUri, articleTitleRegex, null);
+                        if (text == null)
                         {
-                            var comment = new Comment
-                            {
-                                Json = articleCommentJsonText,
-                                Author = articleCommentJsonObject["data"][0]["items"][commentId]["un"].Value<string>(),
-                                Timestamp = Constants.CommentBaseDateTime.AddSeconds(
-                                    articleCommentJsonObject["data"][0]["items"][commentId]["cdatetime"].Value<int>()),
-                                Content =
-                                    articleCommentJsonObject["data"][0]["items"][commentId]["content"].Value<string>()
-                            };
-
-                            article.Comments.Add(comment);
+                            continue;
                         }
+
+                        // Get article
+                        var article = new Article();
+                        article.Title = articleTitleRegex.Match(text).Groups[1].Value;
+                        article.HtmlContent = articleHtmlContentRegex.Match(text).Groups[1].Value;
+
+                        // Get article comments (by json, not regex)
+                        article.Comments = new List<Comment>();
+                        var articleId = articleLink.TrimEnd('/').Substring(articleLink.TrimEnd('/').LastIndexOf('/') + 1);
+                        var articleCommentJsonText = GetCommentJsonText(articleId, 0, 1);
+                        var articleCommentJsonObject = JObject.Parse(articleCommentJsonText);
+                        var articleCommentCount = articleCommentJsonObject["data"][0]["total_count"].Value<int>();
+
+                        if (articleCommentCount > 0)
+                        {
+                            articleCommentJsonText = GetCommentJsonText(articleId, 0, articleCommentCount);
+                            articleCommentJsonObject = JObject.Parse(articleCommentJsonText);
+                            for (int commentId = 0; commentId < articleCommentCount; commentId++)
+                            {
+                                var comment = new Comment
+                                {
+                                    Json = articleCommentJsonText,
+                                    Author = articleCommentJsonObject["data"][0]["items"][commentId]["un"].Value<string>(),
+                                    Timestamp = Constants.CommentBaseDateTime.AddSeconds(
+                                        articleCommentJsonObject["data"][0]["items"][commentId]["cdatetime"].Value<int>()),
+                                    Content =
+                                        articleCommentJsonObject["data"][0]["items"][commentId]["content"].Value<string>()
+                                };
+
+                                article.Comments.Add(comment);
+                            }
+                        }
+
+                        // Add into result
+                        articles.Add(article);
                     }
-
-                    // Add into result
-                    articles.Add(article);
                 }
-            }
 
-            var spaceName = spaceLink.TrimEnd('/').Substring(spaceLink.TrimEnd('/').LastIndexOf('/') + 1);
-            using (var fileStream = new FileStream(@".\Archive\" + spaceName + ".json", FileMode.Create, FileAccess.Write, FileShare.None))
-            {
-                using (var streamWriter = new StreamWriter(fileStream))
+                var spaceName = spaceLink.TrimEnd('/').Substring(spaceLink.TrimEnd('/').LastIndexOf('/') + 1);
+                using (var fileStream = new FileStream(@".\Archive\" + spaceName + ".json", FileMode.Create, FileAccess.Write, FileShare.None))
                 {
-                    streamWriter.Write(JsonConvert.SerializeObject(articles));
+                    using (var streamWriter = new StreamWriter(fileStream))
+                    {
+                        streamWriter.Write(JsonConvert.SerializeObject(articles));
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Crawling Failed, exception: " + ex.Message);
+                return;
+            }
+
+            MessageBox.Show("Crawling Finished!");
         }
 
         private void ButtonLoadFromLocal_Click(object sender, RoutedEventArgs e)
